8de06ddc494ebd20126c400ebe49c5019366cb53
Sergey Osipov <sery.osipow@yandex.ru>
2024-07-02 02:56:33 +0300

    changed the structure, removed the slider, and sketched out styles for the form

--- src/index.js
+++ src/index.js
@@ -8,20 +8,20 @@ import Swiper from 'swiper';
 import { Autoplay, EffectFade } from 'swiper/modules';
 import 'swiper/swiper-bundle.css';
 
-document.addEventListener("DOMContentLoaded", function() {
-  document.body.classList.remove('page__loading');
-  document.body.classList.add('page__loaded');
+/*
 
-  const mySwiper = new Swiper('.swiper-container', {
-    modules: [Autoplay, EffectFade],
-    loop: true,
-    autoplay: {
-      delay: 3000,
-    },
-    effect: 'fade',
-    fadeEffect: {
-      crossFade: true
-    },
-    allowTouchMove: false, 
-  });
+Временно отключен.
+
+const mySwiper = new Swiper('.swiper-container', {
+  modules: [Autoplay, EffectFade],
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  allowTouchMove: false, 
 });
+*/
